29a57aa284370b8fdf18e772a65a9fc0f37867d7
skalogerakis <skalogerakis97@gmail.com>
2021-05-05 01:01:04 +0300

    Created function to transform h5 files into csv

--- initTests.py
+++ initTests.py
@@ -4,7 +4,8 @@ import os
 import glob
 
 
-def hd5_info_parser():
+# Initial effort. Try and parse a random .h5 file
+def hd5_single_random_file_parser():
     # Open an h5 file in read mode
     h5 = hdf5_getters.open_h5_file_read(
         '/home/skalogerakis/Documents/MillionSong/MillionSongSubset/A/M/G/TRAMGDX12903CEF79F.h5')
@@ -31,7 +32,6 @@ def hd5_info_parser():
     print("Artist Name -- ", hdf5_getters.get_artist_name(h5))
     print("Release -- ", hdf5_getters.get_release(h5))
     print("Release 7DigitalID -- ", hdf5_getters.get_release_7digitalid(h5))
-    print("Release -- ", hdf5_getters.get_release(h5))
     print("Song ID -- ", hdf5_getters.get_song_id(h5))
     print("Song Hotness -- ", hdf5_getters.get_song_hotttnesss(h5))
     print("Track 7Digital -- ", hdf5_getters.get_track_7digitalid(h5))
@@ -88,6 +88,95 @@ def hd5_info_parser():
     h5.close()  # close h5 when completed in the end
 
 
+# Following a given directory parse all files into a csv file
+def complete_hd5_to_csv(basedir):
+    ext = '.h5'  # Get all files with extension .h5
+
+    # Header title. Essentially it is a schema for all the following songs
+    header = ['Title', 'Artist familiarity', 'Artist hotness', 'Artist ID', 'Artist mbID', 'Artist playmeid',
+              'Artist 7DigitalID', 'Artist latitude', 'Artist longitude', 'Artist location', 'Artist Name',
+              'Release', 'Release 7DigitalID', 'Song ID', 'Song Hotness', 'Track 7Digital', 'Analysis sample rate',
+              'Audio md5', 'Danceability', 'Duration', 'End of Fade', 'Energy', 'Key', 'Key Confidence', 'Loudness',
+              'Mode', 'Mode Confidence', 'Start of fade out', 'Tempo', 'Time signature', 'Time signature confidence',
+              'Track ID', 'Year']
+
+    with open('Tester.csv', 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=';')
+
+        # writing the header line. This line contains the schema of the data
+        csv_writer.writerow(header)
+
+        # Read all files from the given directories
+        for root, dirs, files in os.walk(basedir):
+            files = glob.glob(os.path.join(root, '*' + ext))
+            print(files)
+
+            for f in files:
+                h5 = hdf5_getters.open_h5_file_read(f)
+
+                # Write as row all elements. NOTE: Only the serialized elements are parsed and not arrays
+                csv_writer.writerow([hdf5_getters.get_title(h5), hdf5_getters.get_artist_familiarity(h5),
+                                     hdf5_getters.get_artist_hotttnesss(h5), hdf5_getters.get_artist_id(h5),
+                                     hdf5_getters.get_artist_mbid(h5), hdf5_getters.get_artist_playmeid(h5),
+                                     hdf5_getters.get_artist_7digitalid(h5), hdf5_getters.get_artist_latitude(h5),
+                                     hdf5_getters.get_artist_longitude(h5), hdf5_getters.get_artist_location(h5),
+                                     hdf5_getters.get_artist_name(h5), hdf5_getters.get_release(h5),
+                                     hdf5_getters.get_release_7digitalid(h5), hdf5_getters.get_song_id(h5),
+                                     hdf5_getters.get_song_hotttnesss(h5),
+                                     hdf5_getters.get_track_7digitalid(h5), hdf5_getters.get_analysis_sample_rate(h5),
+                                     hdf5_getters.get_audio_md5(h5), hdf5_getters.get_danceability(h5),
+                                     hdf5_getters.get_duration(h5), hdf5_getters.get_end_of_fade_in(h5),
+                                     hdf5_getters.get_energy(h5), hdf5_getters.get_key(h5),
+                                     hdf5_getters.get_key_confidence(h5), hdf5_getters.get_loudness(h5),
+                                     hdf5_getters.get_mode(h5), hdf5_getters.get_mode_confidence(h5),
+                                     hdf5_getters.get_start_of_fade_out(h5), hdf5_getters.get_tempo(h5),
+                                     hdf5_getters.get_time_signature(h5),
+                                     hdf5_getters.get_time_signature_confidence(h5),
+                                     hdf5_getters.get_track_id(h5), hdf5_getters.get_year(h5)
+                                     ])
+
+                # For debugging purposes. Everything as expected
+                # print()
+                # print("Num of songs -- ", hdf5_getters.get_num_songs(h5))  # One song per file
+                # print("Title -- ", hdf5_getters.get_title(h5))  # Print the title of a specific h5 file
+                # print("Artist familiarity -- ", hdf5_getters.get_artist_familiarity(h5))
+                # print("Artist hotness -- ", hdf5_getters.get_artist_hotttnesss(h5))
+                # print("Artist ID -- ", hdf5_getters.get_artist_id(h5))
+                # print("Artist mbID -- ", hdf5_getters.get_artist_mbid(h5))
+                # print("Artist playmeid -- ", hdf5_getters.get_artist_playmeid(h5))
+                # print("Artist 7DigitalID -- ", hdf5_getters.get_artist_7digitalid(h5))
+                # print("Artist latitude -- ", hdf5_getters.get_artist_latitude(h5))
+                # print("Artist longitude -- ", hdf5_getters.get_artist_longitude(h5))
+                # print("Artist location -- ", hdf5_getters.get_artist_location(h5))
+                # print("Artist Name -- ", hdf5_getters.get_artist_name(h5))
+                # print("Release -- ", hdf5_getters.get_release(h5))
+                # print("Release 7DigitalID -- ", hdf5_getters.get_release_7digitalid(h5))
+                # print("Song ID -- ", hdf5_getters.get_song_id(h5))
+                # print("Song Hotness -- ", hdf5_getters.get_song_hotttnesss(h5))
+                # print("Track 7Digital -- ", hdf5_getters.get_track_7digitalid(h5))
+                # print("Analysis sample rate -- ", hdf5_getters.get_analysis_sample_rate(h5))
+                # print("Audio md5 -- ", hdf5_getters.get_audio_md5(h5))
+                # print("Danceability -- ", hdf5_getters.get_danceability(h5))
+                # print("Duration -- ", hdf5_getters.get_duration(h5))
+                # print("End of Fade -- ", hdf5_getters.get_end_of_fade_in(h5))
+                # print("Energy -- ", hdf5_getters.get_energy(h5))
+                # print("Key -- ", hdf5_getters.get_key(h5))
+                # print("Key Confidence -- ", hdf5_getters.get_key_confidence(h5))
+                # print("Loudness -- ", hdf5_getters.get_loudness(h5))
+                # print("Mode -- ", hdf5_getters.get_mode(h5))
+                # print("Mode Confidence -- ", hdf5_getters.get_mode_confidence(h5))
+                # print("Start of fade out -- ", hdf5_getters.get_start_of_fade_out(h5))
+                # print("Tempo -- ", hdf5_getters.get_tempo(h5))
+                # print("Time signature -- ", hdf5_getters.get_time_signature(h5))
+                # print("Time signature confidence -- ", hdf5_getters.get_time_signature_confidence(h5))
+                # print("Track ID -- ", hdf5_getters.get_track_id(h5))
+                # # print("Artist mbtags -- ", hdf5_getters.get_artist_mbtags(h5))
+                # # print("Artist mbtags count -- ", hdf5_getters.get_artist_mbtags_count(h5))
+                # print("Year -- ", hdf5_getters.get_year(h5))
+
+                h5.close()
+
+
 # Print all titles
 def print_all_titles(basedir):
     ext = '.h5'  # Get all files with extension .h5
@@ -103,5 +192,6 @@ def print_all_titles(basedir):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    hd5_info_parser()
-    print_all_titles('/home/skalogerakis/Documents/MillionSong/MillionSongSubset/A/M/G/')
+    # hd5_single_random_file_parser()
+    # print_all_titles('/home/skalogerakis/Documents/MillionSong/MillionSongSubset/A/M/G/')
+    complete_hd5_to_csv('/home/skalogerakis/Documents/MillionSong/MillionSongSubset/A/M/G/')
